9dc886a7a25d68a018bae8d85fec5acd6eac4d28
Ronald Sumbayak <ronaldsumbayak611@gmail.com>
2024-12-12 20:18:49 +0700

    Fix update schema retrieval reference to current hour

--- update-item.py
+++ update-item.py
@@ -270,7 +270,7 @@ def get_sales_by_category(branch_name, final=False):
 def get_dynamic_interval():
     with open('interval_schema', 'r') as file:
         interval_schema = [int(line.strip()) for line in file]
-        return interval_schema[a]
+        return interval_schema[datetime.datetime.now().hour]
 
 
 def get_seconds_to_sleep():
